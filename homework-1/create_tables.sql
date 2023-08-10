44f171139bdd0539e6aedede8f511300c50c78bd
Trashcorp567 <Trashcorp567@gmail.com>
2023-08-10 11:49:22 +0300

    homework_1D

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+-- Таблица customers
+CREATE TABLE customers
+(
+    customer_id text PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name text
+);
+
+-- Таблица employee
+CREATE TABLE employee
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+-- Таблица orders
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id text REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employee(employee_id) NOT NULL,
+	order_date date,
+	ship_city text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,49 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+# Параметры подключения к базе данных
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='jj15mnbl'
+)
+cur = conn.cursor()
+
+# Загрузка данных в таблицу employees
+with open('north_data/employees_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Пропустить заголовок CSV-файла
+    for row in reader:
+        employee_id, first_name, last_name, title, birth_date, notes = row
+        cur.execute(
+            "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)",
+            (employee_id, first_name, last_name, title, birth_date, notes)
+        )
+
+# Загрузка данных в таблицу customers
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Пропустить заголовок CSV-файла
+    for row in reader:
+        customer_id, company_name, contact_name = row
+        cur.execute(
+            "INSERT INTO customers VALUES (%s, %s, %s)",
+            (customer_id, company_name, contact_name)
+        )
+
+# Загрузка данных в таблицу orders
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Пропустить заголовок CSV-файла
+    for row in reader:
+        order_id, customer_id, employee_id, order_date, ship_city = row
+        cur.execute(
+            "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+            (order_id, customer_id, employee_id, order_date, ship_city)
+        )
+
+conn.commit()
+cur.close()
+conn.close()
